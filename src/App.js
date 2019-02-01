73533c6892ee6e1045aecef257283357df3b7dbf
yuetongfeng <ryfprivate@outlook.com>
2019-02-01 23:09:25 +1100

    Added Header and Footer functional components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Header from "./weather/Header";
+import Footer from "./weather/Footer";
+
 class App extends Component {
   render() {
     return <div className="App">My Weather App</div>;
---
+++ src/weather/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header>
+      <img className="header__logo" src="icon/logo.png" />
+      <h1 className="header__title">Weather Channel</h1>
+    </header>
+  );
+}
